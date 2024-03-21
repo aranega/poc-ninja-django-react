4604e1647511cf0844fb6045c9d704ceb7171f74
aranega <vincent.aranega@gmail.com>
2024-03-21 08:30:41 -0600

    Change export of openapi.json by default of django ninja

--- generate-binding.bash
+++ generate-binding.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Generates the openAPI specification
-(cd backend && python manage.py generate)
+OPENAPI_FOLDER="openapi/openapi.json"
+(cd backend && python manage.py export_openapi_schema --output "${OPENAPI_FOLDER}" --indent 2)
 
 
 # Generates the typescript API binding
